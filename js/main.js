3a53e2365eef17d83ed09d3f536b44d4d7a8a438
elvisiacobescu <elvis.iacobescu@info.uaic.ro>
2016-04-09 12:03:09 +0300

    log in
    appdate

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function () {}
   //functioneaza o singura data
 document.getElementById("edit1").addEventListener("click",function (){
 document.getElementById("prezent").className+=" micsoreaza";
@@ -17,7 +17,7 @@ document.getElementById("iner_form").innerHTML+="<input type='radio' name='sex' 
 document.getElementById("iner_form").innerHTML+="numartelefon: <br><input type='number' value='07' ></br>";
 document.getElementById("iner_form").innerHTML+="adresa:</br><input type='text' value='bla bla'></br>";
 document.getElementById("iner_form").innerHTML+="oras:</br><input type='text' value='bacau'></br>";
-document.getElementById("iner_form").innerHTML+="salariu dorit in euro:<br><input type='number' value='1000000'></br>";
+document.getElementById("iner_form").innerHTML+="salariu dorit in euro:<br><input type='number' value=''></br>";
 document.getElementById("iner_form").innerHTML+="";
 document.getElementById("iner_form").innerHTML+="<div id='save1'><span class='savetext'>SAVE</span></div>";
 //am incercat sa
@@ -56,11 +56,6 @@ document.getElementById("save1").addEventListener("click", function (){
 
 });
 // var buton=document.getElementById("save1");
-}
-
-
-
-
 // document.getElementById('profile_picture').addEventListener("click",function(){
 // //trebuie facuta aploudul unei poze
 // }
