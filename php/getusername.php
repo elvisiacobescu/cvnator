f9bd77dc927233c19da1f4cf396c55f0625eeb2a
VITALAIDER <VITALAIDER@GMAIL.COM>
2016-04-25 22:51:01 +0300

    getusername.php

---
+++ php/getusername.php
@@ -0,0 +1,34 @@
+<?php
+
+$obj = $_GET['obj'];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cvnator";
+//conect to the data baze
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//else echo "Connected successfully";
+$json = json_decode($obj, true);// decode the JSON into an associative array
+//echo '<pre>' . print_r($json, true) . '</pre>';
+// echo 'what ever '.$obj;
+$idUser=$json['idUser'];
+
+
+$query = sprintf("SELECT email FROM user_data
+    WHERE user_id=". "'".$idUser."'");
+$rezult=$conn-> query($query);
+if ($rezult->num_rows>0){
+while($row = $rezult->fetch_assoc()) {
+    echo  $row["name"];
+}
+} else {
+echo "0 results";
+}
+
+?>
