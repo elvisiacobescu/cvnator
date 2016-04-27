82cb1605e0e3721dec8cecd733f2a5df24e4b153
elvisiacobescu <elvis.iacobescu@info.uaic.ro>
2016-04-27 23:29:38 +0300

    c

--- js/firstload.js
+++ js/firstload.js
@@ -19,23 +19,24 @@ window.onload = function () {
    }
 
   asckname();
-  //document.getElementById('logout').addEventListener("click",test());
- // function askpersonaldata(){
- //   var hr=new XMLHttpRequest();
- //   var url="php/personaldata.php";
- //   hr.open("GET",url,true);
- //   hr.send();
- //   hr.onreadystatechange = function()
- //   {
- //       if(hr.readyState == 4 && hr.status == 200)
- //       {
- //         var responce=hr.responseText;
- //         mail=responce;
- //         document.getElementById('nume-utilizator-helo').innerHTML +=  responce;
- //         }
- //     }
- // }
- //
- // askpersonaldata();
+
+
+   function askpersonaldata(){
+   var hr=new XMLHttpRequest();
+   var url="php/personaldata.php";
+   hr.open("GET",url,true);
+   hr.send();
+   hr.onreadystatechange = function()
+   {
+       if(hr.readyState == 4 && hr.status == 200)
+       {
+         var responce=hr.responseText;
+         mail=responce;
+         document.getElementById('eror-respoce').innerHTML +=  responce;
+         }
+     }
+ }
+
+ askpersonaldata();
 
 }
--- php/personaldata.php
+++ php/personaldata.php
@@ -7,8 +7,9 @@ $dbname = "cvnator";
 $arr=array();
 $userid=$_COOKIE["cookie_user_id"];
 $conn = new mysqli($servername, $username, $password,$dbname);
-$getid=sprintf("SELECT * FROM date_peronale where user_id=$userid");
-$rezult=$conn-> query($getid);
+
+$getid=sprintf("SELECT * FROM date_peronale where user_id=".$userid);
+$rezult= $conn ->query($getid);
 if ($rezult->num_rows>0){
 while($row = $rezult->fetch_assoc()) {
   $nume=$row["nume"];
@@ -31,7 +32,7 @@ while($row = $rezult->fetch_assoc()) {
   $obiectiv=$row["obiectiv"];
   //aici trebuie facut un array
   $arr = array('nume' => $nume ,'prenume' => $prenume,'sex' => $row["sex"],'varsta' => $row["varsta"],'nr_telefon' => $row["nr_telefon"],'cale_poza' => $row["cale_poza"],'adresa' => $row["adresa"],'oras' => $row["oras"],'salariu_dorit' => $row["salariu_dorit"],'tip_job' => $row["tip_job"],'nivel_cariera' => $row["nivel_cariera"],'domeniu' => $row["domeniu"],'post_dorit' => $row["post_dorit"],'relocare' => $row["relocare"],'obiectiv' => $row["obiectiv"], );
-  
+
 
  }
 }
