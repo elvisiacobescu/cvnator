301e6d6c0c226c52fbaa81099023fe42b53b9123
elvisiacobescu <elvis.iacobescu@info.uaic.ro>
2016-04-27 23:30:54 +0300

    1

--- php/personaldata.php
+++ php/personaldata.php
@@ -31,9 +31,8 @@ while($row = $rezult->fetch_assoc()) {
   $relocare=$row["relocare"];
   $obiectiv=$row["obiectiv"];
   //aici trebuie facut un array
-  $arr = array('nume' => $nume ,'prenume' => $prenume,'sex' => $row["sex"],'varsta' => $row["varsta"],'nr_telefon' => $row["nr_telefon"],'cale_poza' => $row["cale_poza"],'adresa' => $row["adresa"],'oras' => $row["oras"],'salariu_dorit' => $row["salariu_dorit"],'tip_job' => $row["tip_job"],'nivel_cariera' => $row["nivel_cariera"],'domeniu' => $row["domeniu"],'post_dorit' => $row["post_dorit"],'relocare' => $row["relocare"],'obiectiv' => $row["obiectiv"], );
-
-
+  $arr = array('nume' => $nume ,'prenume' => $prenume,'sex' => $row["sex"],'varsta' => $row["varsta"],'nr_telefon' => $row["nr_telefon"],'cale_poza' => $row["cale_poza"],'adresa' => $row["adresa"],'oras'
+  => $row["oras"],'salariu_dorit' => $row["salariu_dorit"],'tip_job' => $row["tip_job"],'nivel_cariera' => $row["nivel_cariera"],'domeniu' => $row["domeniu"],'post_dorit' => $row["post_dorit"],'relocare' => $row["relocare"],'obiectiv' => $row["obiectiv"], );
  }
 }
 echo json_encode($arr);
