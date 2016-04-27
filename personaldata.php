6f6db461f06f8faed2fcc52d47d1e5062e8b1240
VITALAIDER <VITALAIDER@GMAIL.COM>
2016-04-27 22:52:57 +0300

    personaldata

--- personaldata.php
+++ personaldata.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "cvnator";
 //conect to the data baze
-$centralstage=array();
+$arr=array();
 $userid=$_COOKIE["cookie_user_id"];
 $conn = new mysqli($servername, $username, $password,$dbname);
 $getid=sprintf("SELECT * FROM date_peronale where user_id=$userid");
@@ -30,15 +30,11 @@ while($row = $rezult->fetch_assoc()) {
   $relocare=$row["relocare"];
   $obiectiv=$row["obiectiv"];
   //aici trebuie facut un array
-  $arr = array('start' => $start ,'stop' => $stop,'car' => $car,'date' => $date,'name' => $name,'price' => $price );
-  array_push($centralstage,$arr);
+  $arr = array('nume' => $nume ,'prenume' => $prenume,'sex' => $row["sex"],'varsta' => $row["varsta"],'nr_telefon' => $row["nr_telefon"],'cale_poza' => $row["cale_poza"],'adresa' => $row["adresa"],'oras' => $row["oras"],'salariu_dorit' => $row["salariu_dorit"],'tip_job' => $row["tip_job"],'nivel_cariera' => $row["nivel_cariera"],'domeniu' => $row["domeniu"],'post_dorit' => $row["post_dorit"],'relocare' => $row["relocare"],'obiectiv' => $row["obiectiv"], );
+  
 
  }
 }
-echo json_encode($centralstage);
+echo json_encode($arr);
 
-
-//$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
-
-//echo json_encode($arr);
 ?>
