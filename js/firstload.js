8a139dcff58c2b94b14bcd6990f6e7213495abe3
elvisiacobescu <elvis.iacobescu@info.uaic.ro>
2016-04-27 21:38:13 +0300

    schimbare minora

--- js/firstload.js
+++ js/firstload.js
@@ -4,7 +4,7 @@ window.onload = function () {
  var responce='';
  function asckname(){
    var hr=new XMLHttpRequest();
-   var url="php/getusername.php?";
+   var url="php/getusername.php";
    hr.open("GET",url,true);
    hr.send();
    hr.onreadystatechange = function()
@@ -21,7 +21,19 @@ window.onload = function () {
   asckname();
   //document.getElementById('logout').addEventListener("click",test());
  function askpersonaldata(){
-
+   var hr=new XMLHttpRequest();
+   var url="php/personaldata.php";
+   hr.open("GET",url,true);
+   hr.send();
+   hr.onreadystatechange = function()
+   {
+       if(hr.readyState == 4 && hr.status == 200)
+       {
+         var responce=hr.responseText;
+         mail=responce;
+         document.getElementById('nume-utilizator-helo').innerHTML +=  responce;
+         }
+     }
  }
 
  askpersonaldata();
