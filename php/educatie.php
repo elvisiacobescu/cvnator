642d373be6ddbcbd73c878393dcf55457d46d03d
elvisiacobescu <elvis.iacobescu@info.uaic.ro>
2016-04-29 14:57:07 +0300

    solvd
    am facut sa aduca toate datele din  tabel

--- php/educatie.php
+++ php/educatie.php
@@ -13,15 +13,16 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 $getid=sprintf("SELECT * FROM educatie where user_id=".$userid);
 $rezult= $conn ->query($getid);
 
-
+$ary=array();
 if ($rezult->num_rows>0){
   $nrexperiente=$rezult->num_rows;
 while($row = $rezult->fetch_assoc()) {
 
   //aici trebuie facut un array
   $arr = array('nrexperiente' => $nrexperiente,'entry_id' => $row["entry_id"] ,'start' => $row["start"],'stop' => $row["stop"],'nume_institutie' => $row["nume_institutie"],'oras' => $row["oras"],'profil' => $row["profil"]);
+array_push($ary,$arr);
  }
 }
-echo json_encode($arr);
+echo json_encode($ary);
 
 ?>
