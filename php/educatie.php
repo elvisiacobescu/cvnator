c921ea27fcd31498cdc1daa9704da0218db979e2
VITALAIDER <VITALAIDER@GMAIL.COM>
2016-04-29 15:02:26 +0300

    wdasbghsrvsdc

--- php/educatie.php
+++ php/educatie.php
@@ -10,17 +10,21 @@ $userid=$_COOKIE["cookie_user_id"];
 
 $conn = new mysqli($servername, $username, $password,$dbname);
 
-$getid=sprintf("SELECT * FROM educatie where user_id=".$userid);
+
+
+$getid=sprintf("SELECT * FROM educatie where user_id=".$userid."and entry_id=");
 $rezult= $conn ->query($getid);
-
-
 if ($rezult->num_rows>0){
   $nrexperiente=$rezult->num_rows;
+  for ($i=1;$i<=2;$i++)
+  {
+
 while($row = $rezult->fetch_assoc()) {
 
   //aici trebuie facut un array
-  $arr = array('nrexperiente' => $nrexperiente,'entry_id' => $row["entry_id"] ,'start' => $row["start"],'stop' => $row["stop"],'nume_institutie' => $row["nume_institutie"],'oras' => $row["oras"],'profil' => $row["profil"]);
- }
+  $arr= array('nr' => $i,'entry_id' => $row["entry_id"] ,'start' => $row["start"],'stop' => $row["stop"],'nume_institutie' => $row["nume_institutie"],'oras' => $row["oras"],'profil' => $row["profil"]);
+ 		} //while
+	} //for
 }
 echo json_encode($arr);
 
