a3edc5b524c120136c47a9c242b790009889d23a
MateiciucDaniel <danield.mateiciuc@gmail.com>
2016-05-12 00:15:25 +0300

    e viu

--- php/send_data.php
+++ php/send_data.php
@@ -2,7 +2,7 @@
 	if(isset($_POST["em"])){
 		//include_once("php_files/connect_to_database.php");
 		
-		$em = $_POST["em"];
+		$em = mysqli_real_escape_string($database,$_POST["em"]);
 		$pass = $_POST["p"];
 		// mai verificam o data?
 		
@@ -18,19 +18,12 @@
 			echo "Email in uz."; die();
 		}
 		else{
-			$id_user = 1;
-			$sql = "SELECT max(user_id) FROM user_data LIMIT 1";
-			$query =  mysqli_query($database,$sql);
-			if (mysqli_num_rows($query) > 0) {
-				while($row = mysqli_fetch_assoc($query)) {
-        			$id_user = $row["user_id"];
-   			 		}
-			}
 			
-			$id_user = $id_user + 1;
-			$sql = "INSERT INTO user_data (user_id, email, parola) VALUES('$id_user','$em','$pass')";
+			$sql = "INSERT INTO user_data ( email, parola) VALUES('$em','$pass')";
 			$query = mysqli_query($database, $sql); 
-			$uid = mysqli_insert_id($database);
+			echo "Inregistrare efectuata cu succes!";
+			header("location: index.php");
+			exit();
 		}
 	}
 ?>
