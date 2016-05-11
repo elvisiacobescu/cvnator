0f5199514c5d54a11ef3c1ede6e6260fe8c4e124
MateiciucDaniel <danield.mateiciuc@gmail.com>
2016-05-11 23:19:33 +0300

    dsadsadsad

--- register.php
+++ register.php
@@ -5,12 +5,9 @@
 		header("location: cv.html?email=".$_SESSION["session_email"]);
 		exit();
 	}
-<<<<<<< HEAD:register.php
+
     include_once("php/verify_email.php");
     include_once("php/send_data.php");
-=======
-	include_once("php/register.php")
->>>>>>> origin/test:register.html
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,9 +25,9 @@
     <div>Email Address:</div>
     <input id="email" type="text" onfocus="emptyElement('status')" onkeyup="restrict('email')" maxlength="88">
     <div>Create Password:</div>
-    <input id="parola" type="password" onfocus="emptyElement('status')" maxlength="16">
+    <input id="pass1" type="password" onfocus="emptyElement('status')" maxlength="16">
     <div>Confirm Password:</div>
-    <input id="parola2" type="password" onfocus="emptyElement('status')" maxlength="16">
+    <input id="pass2" type="password" onfocus="emptyElement('status')" maxlength="16">
     <br>
     <button id="signupbtn" onclick="signup()">Create Account</button>
     <span id="status"></span>
