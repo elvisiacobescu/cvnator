e9a6310c9bb6c39135537d8277b255adcb77d177
VITALAIDER <VITALAIDER@GMAIL.COM>
2016-05-27 14:02:13 +0300

    ultimelee

--- php/modificaexperienta.php
+++ php/modificaexperienta.php
@@ -20,54 +20,18 @@ $departament=$json["departament"];
 $descrierea_jobului=$json["descrierea_jobului"];
 $entry_id=$json["entry_id"];
 
-// //
-// if($sql=$conn->prepare(
-// 	"UPDATE experienta SET 
-//  	start =? , stop=? , nume_companie=? , domeniu=? , departament=? , descrierea_jobului=? where user_id=? AND entry_id=? " )
-// 	)
-// 	{
+//
+if($sql=$conn->prepare(
+	"UPDATE experienta SET 
+ 	start =? , stop=? , nume_companie=? , domeniu=? , departament=? , descrierea_jobului=? where user_id=? AND entry_id=? " )
+	)
+	{
 
-// $sql->bind_param('ssssssii',$start,$stop,$nume_companie,$domeniu, $departament,$descrierea_jobului,$userid,$entry_id);
-// $sql->execute() ;
+$sql->bind_param('ssssssii',$start,$stop,$nume_companie,$domeniu, $departament,$descrierea_jobului,$userid,$entry_id);
+$sql->execute() ;
 
-// echo 'succes';
-// } 
-// else echo "erruare: " . $conn->error;
-// //
-
-
-
-
-// 	if( $sql=$conn->prepare( "UPDATE experienta SET start =? , stop=? , nume_companie=? , domeniu=? , departament=? , descrierea_jobului=? where user_id=? AND entry_id=? " )
-// 	)
-// 	{
-
-// 	$sql->bind_param('ssssssii', $start, $stop, $nume_companie, $domeniu, 
-// 		$departament, $descrierea_jobului, $userid, $entry_id );
-
-// $sql->execute() ;
-
-// echo "succes";
-// } 
-
-
-
-// else echo "erruare: " . $conn->error;
-
-$sql=sprintf("UPDATE experienta SET 
-	start ="."'".$json["start"]."'".
-" , stop="."'".$json["stop"]."'".
-" , nume_companie="."'".$json["nume_companie"]."'".
-" , domeniu="."'".$json["domeniu"]."'".
-" , departament="."'".$json["departament"]."'".
-" , descrierea_jobului="."'".$json["descrierea_jobului"]."'".
-" where user_id=".$userid. " AND entry_id='".$json["entry_id"]."' ");
-
-if ($conn->query($sql) === TRUE) {
-		echo "succes";
-
-} else {
-    echo "erruare: " . $conn->error;
-}
+echo 'succes';
+} 
+else echo "erruare: " . $conn->error;
 
 ?>
--- php/modificapersonaldata.php
+++ php/modificapersonaldata.php
@@ -54,36 +54,6 @@ echo 'succes';
 } 
 else echo 'eroare';
 
-// $sql=sprintf(
-// 	"INSERT INTO date_personale 
-// 	(user_id,nume,prenume,sex,varsta,nr_telefon,email,cale_poza,adresa,oras,salariu_dorit,tip_job,nivel_cariera,domeniu,post_dorit,relocare,obiectiv)
-// VALUES (
-// '".$userid."'"."
-// ,"."'".$json["nume"]."'"."
-// ,'".$json["prenume"]."'"."
-// ,"."'".$json["sex"]."'"."
-// ,"."'".$json["varsta"]."'"."
-// ,"."'".$json["nr_telefon"]."'"."
-// ,'".$json["email"]."'"."
-// ,"."'".$json["cale_poza"]."'"."
-// ,'".$json["adresa"]."'"."
-// ,"."'".$json["oras"]."'"."
-// ,"."'".$json["salariu_dorit"]."'"."
-// ,"."'".$json["tip_job"]."'"."
-// ,'".$json["nivel_cariera"]."'"."
-// ,"."'".$json["domeniu"]."'"."
-// ,"."'".$json["post_dorit"]."'"."
-// ,"."'".$json["relocare"]."'"."
-// ,"."'".$json["obiectiv"]."')"
-// 	);
-// if ($conn->query($sql) === TRUE) {
-// 		echo "succes";
-
-// } else {
-//     echo "erruare: " . $conn->error;
-// }
-
-
  }
  else 
 // daca exista, facem update
@@ -114,33 +84,6 @@ $sql->execute() ;
 echo 'succes';
 } 
 else echo "erruare: " . $conn->error;
-
-// $sql=sprintf("UPDATE date_personale SET nume="."'".$json["nume"]."'".
-// " , prenume ="."'".$json["prenume"]."'".
-// " , sex="."'".$json["sex"]."'".
-// " , varsta="."'".$json["varsta"]."'".
-// " , nr_telefon="."'".$json["nr_telefon"]."'".
-// " , adresa="."'".$json["adresa"]."'".
-// " , oras="."'".$json["oras"]."'".
-// " , salariu_dorit="."'".$json["salariu_dorit"]."'".
-// " , tip_job="."'".$json["tip_job"]."'".
-// " , nivel_cariera="."'".$json["nivel_cariera"]."'".
-// " , domeniu="."'".$json["domeniu"]."'".
-// " , post_dorit="."'".$json["post_dorit"]."'".
-// " , relocare="."'".$json["relocare"]."'".
-// " , obiectiv="."'".$json["obiectiv"]."'".
-// " , email="."'".$json["email"]."'".
-// " where user_id=".$userid);
-// }
-
-
-// if ($conn->query($sql) === TRUE) {
-// 		echo "succes";
-// //		echo "succes".$json["nume"];
-// } else {
-//     echo "erruare: " . $conn->error;
-// }
-
 
 	}
 }
